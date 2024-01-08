688985b2a6fad76709035668d59e7a9a64f025d7
wolf1994722 <herlmercedes@gmail.com>
2024-01-09 03:01:07 +0900

    change api

--- example/urls.py
+++ example/urls.py
@@ -1,14 +1,15 @@
 # example/urls.py
 from django.urls import path
 
-from example.views import index
+# from example.views import index
+
+from . import views
 
 
 urlpatterns = [
-    path('', index),
-    # path('test/', index.tester, name='tester'),
-    path('api/get_prompt/', index.get_prompt, name='get_prompt'),
-    path('api/suggest_input/', index.suggest_input, name='suggest_input'),
-    path('api/generate_assistant_ai/', index.generate_assistant_ai, name='generate_assistant_ai'),
-    path('api/upload_file/', index.upload_json, name='upload_file'),
+    path('', views.index),
+    path('api/get_prompt/', views.get_prompt, name='get_prompt'),
+    path('api/suggest_input/', views.suggest_input, name='suggest_input'),
+    path('api/generate_assistant_ai/', views.generate_assistant_ai, name='generate_assistant_ai'),
+    path('api/upload_file/', views.upload_json, name='upload_file'),
 ]
--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+OPENAI_API_KEY = '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'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^'
 
@@ -55,7 +57,9 @@ ROOT_URLCONF = 'vercel_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'    
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
